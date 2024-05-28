e9c887cdc25caf5f89952ad5ce337a8bdb960fe2
nisar155 <151600693+nisar155@users.noreply.github.com>
2024-05-28 12:15:52 +0530

    Add files via upload

---
+++ js2/condition.js
@@ -0,0 +1,44 @@
+// if(11<10)
+//     {
+//         console.log('11 is lesser');
+
+//     }
+//     else{
+//         console.log('11 is greater');
+//     }
+
+// (15<10)?console.log("15 is lesser"):console.log("15 is greater");
+
+// let time=17;
+// if( time>=1 && time<=11){
+// console.log("Good Morning")
+// }
+// else if(time>=12 && time<=16){
+//     console.log("Good Afternoon")
+// }
+// else if(time>=17 && time<=19){
+//     console.log("Good Evening")
+// }t
+// else if(time>=20 && time<=24)
+//     {
+//     console.log("Good Night")
+// }
+
+let p=[1,2,3,4,5,6,7,8,9,10]
+
+for(let i = 0; i < p.length; i++){
+    if(p[i] % 2 ==0)
+
+    console.log(`${p[i]}is even`)
+
+
+else
+    console.log(`${p[i]}is odd`)
+}
+    
+
+
+
+
+
+
---
+++ js2/loop.js
@@ -0,0 +1,34 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+// for loop
+// let fruits =["Apple","Banana","Grapes","Pineapple"];
+// for (let i=0;i<fruits.length;i++)
+//     {
+//         console.log(fruits[i]);
+//     }
+
+// sum of the array
+
+// const numbers =[1,2,3,4,5];
+// var sum =0;
+// for(let i=0;i<numbers.length;i++)
+//     {
+//     sum= sum + numbers[i]
+   
+// }
+// console.log(sum);
+
+// print multiplication table 1-5
+
+// for(let i = 1; i <= 5; i++){
+// for(let j=1;j<=5;j++){
+//     let p =`${i}*${j}=${i*j}`
+//     console.log(p)
+// }
+// }
+
+    
+    
+
+   
